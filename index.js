1189e1f96eb44e0c5dc3c92d859f3b5092c88817
bouzianes <fcbdreams1991@gmail.com>
2024-08-12 09:48:57 -0400

    string methods

--- index.js
+++ index.js
@@ -1,48 +1,20 @@
-//switch case
-/*let day = 'j' ;
-switch(day) {
-     case 1: 
-     console.log('it is moday')
-     break;
-     case 2: 
-     console.log('it is tuesday')
-     break;
-     case 3: 
-     console.log('it is whensday')
-     break;
-     case 4: 
-     console.log('it is thursday')
-     break;
-     case 5: 
-     console.log('it is friday')
-     break;
-     case 6: 
-     console.log('it is saturday')
-     break;
-     case 7: 
-     console.log('it is sunday')
-     break;
-     default :
-     console.log(`${day} is not a day`)
-}*/
+//string methods
 
-let testScor = 50;
-let letterGrade;
 
-switch(true) {
-    case testScor >= 90:
-    letterGrade = 'A'
-    break
-    case testScor >= 80:
-    letterGrade = 'B'
-    break
-    case testScor >= 70:
-    letterGrade = 'C'
-    break
-    case testScor >= 60:
-    letterGrade = 'D'
-    break
-    default:
-        letterGrade = 'F'
+let userName =' boz';
+let result = userName.startsWith(' ');
+if(result){
+    console.log("your userName cant begin with ' '")
+}else{
+    console.log(userName)
 }
-console.log(letterGrade)
+
+
+userName = userName.trim();
+userName = userName.toUpperCase();
+userName = userName.repeat();
+console.log(userName)
+console.log(userName.charAt(0));
+console.log(userName.indexOf('b'));
+console.log(userName.indexOf('p'));
+console.log(userName.length);
