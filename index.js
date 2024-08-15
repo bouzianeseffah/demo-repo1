1345fedefb756d18b8702c0f01aa59a137b64462
bouzianes <fcbdreams1991@gmail.com>
2024-08-15 09:39:34 -0400

    itro function

--- index.js
+++ index.js
@@ -1,38 +1,35 @@
-// number gassing game
-
-const minNum = 1;
-const maxNum = 100;
-const answer = Math.floor(Math.random() * (maxNum - minNum +1 ) + minNum ) ;
- 
-
-let attempts = 0;
-let guess;
-let running = true;
-while(running){
-
-    guess = window.prompt(`guess a number between ${minNum} - ${maxNum} `) 
-    guess = Number(guess)
-    console.log(typeof guess, guess )
-    if(isNaN(guess)){
-        window.alert('please enter a valid number')
-    }else if (guess < minNum || guess > maxNum) {
-        window.alert('please enter a valid number')
-
+//function number is even or odd
+function isEven(number) {
+    if(number % 2 === 0) {
+        return true
     }
-    else {
-        attempts++;
-        if(guess < answer ) {
-            window.alert('too LOW! try again')
-        }else if (guess > answer) {
-            window.alert('too HIGHT! try again')
-
-        }else {
-            window.alert(`CORRECT! the answer was ${answer} , it took you  ${attempts} attempts`)
-            running = false
-
-        }
+    else{
+      return  false
     }
+}
+console.log(isEven(10))
 
 
+function isEven2(number) {
+    return  number % 2 === 0 ? true : false
+}
+console.log(isEven(11))
+console.log(isEven2(12))
+
+
+function isValidEmail (email) {
+    if(email.includes('@')){
+        return true;
+    }
+    else{
+        return false
+    }
 
 }
+
+function isValidEmail2(email){
+    return email.includes('@') ? true : false
+
+}
+console.log(isValidEmail('sdgsdbsgamil.con'))
+console.log(isValidEmail2('adfvsd@gmail.com'))
