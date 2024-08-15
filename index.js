85b649565159ec7cc2c51ba525798e411df166b4
bouzianes <fcbdreams1991@gmail.com>
2024-08-15 09:14:57 -0400

    guessing number game

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
-//  while loop
-let loggedId = false;
-let userName;
-let password;
+// number gassing game
+
+const minNum = 1;
+const maxNum = 100;
+const answer = Math.floor(Math.random() * (maxNum - minNum +1 ) + minNum ) ;
+ 
+
+let attempts = 0;
+let guess;
+let running = true;
+while(running){
+
+    guess = window.prompt(`guess a number between ${minNum} - ${maxNum} `) 
+    guess = Number(guess)
+    console.log(typeof guess, guess )
+    if(isNaN(guess)){
+        window.alert('please enter a valid number')
+    }else if (guess < minNum || guess > maxNum) {
+        window.alert('please enter a valid number')
+
+    }
+    else {
+        attempts++;
+        if(guess < answer ) {
+            window.alert('too LOW! try again')
+        }else if (guess > answer) {
+            window.alert('too HIGHT! try again')
+
+        }else {
+            window.alert(`CORRECT! the answer was ${answer} , it took you  ${attempts} attempts`)
+            running = false
+
+        }
+    }
 
 
-while(!loggedId){
-    userName = window.prompt('enter your user name');
-    password = window.prompt('enter your user password')
-    if(userName === 'myUserName' && password === 'myPassword'){
-        loggedId = true
-        console.log('you are logged in')
-    }
-    else{
-        console.log('invalid credentials')
-    }
 
 }
-
