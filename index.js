4152734253fed7425cf7b92d7735e5aaf10f54d8
bouzianes <fcbdreams1991@gmail.com>
2024-08-22 09:31:15 -0400

    random password generator

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
-// Dice Roller program
+//RANDOM PASSWORD GENERATOR
+function generatePassword(length, includeLowercase, includeUppercase, includeNumber, includeSymbols ){
+          const lowercaseCharts = 'abcdefghijlmnopqkutwszx';
+          const uppercaseCharts = 'ABCDEFGHIJKOPQLMNKUTSWZX';
+          const numberCharts = '0123456789';
+          const symbolCharts = '@$!%$*()_+^-=';
+          let allowedCharts = '';
+          let password = '';
+          allowedCharts += includeLowercase ? lowercaseCharts : '';
+          allowedCharts += includeUppercase ? uppercaseCharts : '';
+          allowedCharts += includeNumber ? numberCharts : '';
+          allowedCharts += includeSymbols ? symbolCharts : '';
+          //console.log(allowedCharts)
+          if(length <= 0){
+            return `(password length must be at least 1)`
+          }
+          if(allowedCharts.length === 0) {
+            return `(at least 1 set of character needs to be selected)`;
+          }
+          for (let i = 0; i < length; i++) {
+              const randomIndex =Math.floor (Math.random() * allowedCharts.length);
+              password += allowedCharts[randomIndex]
+          }
+         
 
-function rollDice(){
-    const numOfDice = document.getElementById('numOfDice').value;
-    const diceResult = document.getElementById('diceResult');
-    const diceImagies = document.getElementById('diceImages');
-    const values = [];
-    const images = [];
-
-    for (let i = 0; i < numOfDice; i++) {
-      const value =Math.floor( Math.random() * 6) + 1;
-      //console.log(value)
-      values.push(value);
-      images.push(`<img src="/dice_images/${value}.png" >`)
-    }
-    //console.log(values)
-    diceResult.textContent = `dice ${values.join(',  ')}`
-    diceImagies.innerHTML = images.join('')
-
-
-
+           return password;
 }
+const paswordLength = 12;
+const includeLowercase = true;
+const inclideUppercase = true;
+const includeNumber = true;
+const includeSymbols = true;
+
+
+const password =  generatePassword(paswordLength, includeLowercase,
+                                   inclideUppercase,
+                                   includeNumber, includeSymbols);
+
+                                   
+console.log(`generated password: ${password}`)                                   
