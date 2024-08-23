b7e1aee0d549226aa0d344e95c248330732ebaec
bouzianes <fcbdreams1991@gmail.com>
2024-08-23 09:22:50 -0400

    forEach

--- index.js
+++ index.js
@@ -1,40 +1,16 @@
-//RANDOM PASSWORD GENERATOR
-function generatePassword(length, includeLowercase, includeUppercase, includeNumber, includeSymbols ){
-          const lowercaseCharts = 'abcdefghijlmnopqkutwszx';
-          const uppercaseCharts = 'ABCDEFGHIJKOPQLMNKUTSWZX';
-          const numberCharts = '0123456789';
-          const symbolCharts = '@$!%$*()_+^-=';
-          let allowedCharts = '';
-          let password = '';
-          allowedCharts += includeLowercase ? lowercaseCharts : '';
-          allowedCharts += includeUppercase ? uppercaseCharts : '';
-          allowedCharts += includeNumber ? numberCharts : '';
-          allowedCharts += includeSymbols ? symbolCharts : '';
-          //console.log(allowedCharts)
-          if(length <= 0){
-            return `(password length must be at least 1)`
-          }
-          if(allowedCharts.length === 0) {
-            return `(at least 1 set of character needs to be selected)`;
-          }
-          for (let i = 0; i < length; i++) {
-              const randomIndex =Math.floor (Math.random() * allowedCharts.length);
-              password += allowedCharts[randomIndex]
-          }
-         
+// forEach() : mthod used to iterate over the elements 
+//             of an array
+let fruits = ['banana', 'apple', 'orange', 'coconut'];
 
-           return password;
+fruits.forEach(capitalize)
+fruits.forEach(display);
+
+
+function capitalize(element, index, array){
+     array[index] = element.charAt(0).toUpperCase() + element.slice(1) ;
 }
-const paswordLength = 12;
-const includeLowercase = true;
-const inclideUppercase = true;
-const includeNumber = true;
-const includeSymbols = true;
 
 
-const password =  generatePassword(paswordLength, includeLowercase,
-                                   inclideUppercase,
-                                   includeNumber, includeSymbols);
-
-                                   
-console.log(`generated password: ${password}`)                                   
+function display(element){
+  console.log(element)
+}
