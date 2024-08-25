9c5a35c874532bd49d9ea2748e16059ea1fdc431
bouzianes <fcbdreams1991@gmail.com>
2024-08-25 09:12:22 -0400

    arrow fuction

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-//function declaration 
+//Arrow functions
 
-//function hello() {
- //    console.log('hello')
-//}
-//function expressions
+const numbers = [1, 2, 3, 4, 5, 6]
 
-//const hello = function() {
- //    console.log('hello')
-//}
-//hello()
-const numbers = [1, 2, 3, 4, 5, 6];
-const squares = numbers.map(function(element) {
-     return Math.pow(element, 3)
-});
-const isEven = numbers.filter(function(element){
-     return element % 2 === 0;
-})
-console.log(isEven)
-const isOdd = numbers.filter(function(element){
-     return element % 2 !== 0;
-})
-console.log(isOdd)
+
+const squares = numbers.map((element) => Math.pow(element, 2));
+console.log(squares);
+
+const cubes = numbers.map((element) => Math.pow(element, 3));
+console.log(cubes);
+
+const isOdd = numbers.filter((element) => element % 2 === 0);
+console.log(isOdd);
+
+const isEven = numbers.filter((element) => element % 2 !== 0);
+console.log(isEven);
+
+const total = numbers.reduce((accumulator, element) => accumulator + element )
+console.log(total)
