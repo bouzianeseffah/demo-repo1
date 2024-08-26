968f8ccbfd13025a952e33087c99d72765bd2662
bouzianes <fcbdreams1991@gmail.com>
2024-08-26 09:33:43 -0400

    class

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-//Object
+//class
 
 
-const person1 = {
-     fistName: 'boz',
-     lastName: 'sef',
-     age: 32,
-     isEmployed: true,
-     sayHello: function(){console.log('hi')},
+class Product {
+     constructor(name, price){
+          this.name = name;
+          this.price = price;
+     }
+     displayProduct(){
+          console.log(`Product : ${this.name}`)
+          console.log(`price$ ${this.price.toFixed(2)}`)
+     }
+     calculateTotal(salesTaxe) {
+          return this.price + (this.price * salesTaxe)
+     }
 }
+const product1 = new Product('shirt', 19.99);
+const product2 = new Product('pants', 22.50);
+const product3 = new Product('underwear', 100.00);
+const salesTaxes = 0.05;
+product1.displayProduct();
+product2.displayProduct();
+product3.displayProduct();
 
-const person2= {
-     fistName: 'fat',
-     lastName: 'bkr',
-     age: 22,
-     isEmployed: false,
-     eat: () => console.log('hello')
-}
-console.log(person1.fistName)
-console.log(person1.lastName)
-console.log(person1.age)
-console.log(person1.isEmployed)
-person1.sayHello()
-console.log(person2.fistName)
-console.log(person2.lastName)
-console.log(person2.age)
-console.log(person2.isEmployed)
-person2.eat()
+const total = product1.calculateTotal(salesTaxes)
+const total2 = product2.calculateTotal(salesTaxes)
+
+
+console.log(`total price (with tax) $${total.toFixed(2)}`)
+
