bb8b77b3020e04cf264bdb15fa25a10ce52c4df3
bouzianes <fcbdreams1991@gmail.com>
2024-08-26 23:45:12 -0400

    inheritence

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
-// static
+//inheritance
 
+class Animal {
+     alive = true;
 
-class User {
-     static userCount = 0;
-     constructor(userName) {
-          this.userName = userName;
-          User.userCount++
+     eat() {
+          console.log(`this ${this.name} is alive`);
      }
-     static getUserCount (){
-          console.log(`there are ${User.userCount} users online`)
-     }
-     sayHello(){
-          console.log(`hello, my user name is ${this.userName}`)
+     sleep() {
+          console.log(`this ${this.name} is sleeping`);
+     } 
+}
+class Rabbit extends Animal {
+        name = 'rabbit';
+        run(){
+          console.log(`${this.name} is running`);
      }
 }
-const user1 = new User('boz');
-const user2 = new User('anl');
-const user3 = new User('ahm');
-user1.sayHello();
-user2.sayHello();
-user3.sayHello()
-User.getUserCount()
 
+class Fish extends Animal {
+     name = 'fish';
+     swim(){
+          console.log(`${this.name} is swimimg`);
+     }
+     
+}
+class Hawk extends Animal{
+     name = 'hawk';
+     fly(){
+          console.log(`${this.name} is fliyng`)
+     }
+}
 
+const rabbit = new Rabbit();
+const fish = new Fish();
+const hawk = new  Hawk()
+console.log(rabbit.alive)
+rabbit.eat()
+rabbit.sleep()
+rabbit.run()
 
-
-console.log(User.userCount)
-
+console.log(fish.alive)
+fish.eat()
+fish.sleep()
