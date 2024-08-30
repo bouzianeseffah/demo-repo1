ba1bc3aa6cac497afa74ed7b34a046960c7aa37c
bouzianes <fcbdreams1991@gmail.com>
2024-08-30 09:34:31 -0400

    get and set

--- index.js
+++ index.js
@@ -1,56 +1,54 @@
-class Animal {
-    constructor(name, age){
-     this.name = name;
-     this.age = age;
-
+class Person {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
     }
-    move(speed){
-     console.log(`this ${this.name} moves at speed of ${speed}`)
-    }
-}
-
-class Rabit extends Animal{
-     constructor(name, age, runSpeed){
-          super(name, age);
+    set firstName(newFistName) {
+        if(typeof newFistName === 'string' && newFistName.length > 0){
+             this._firstName = newFistName;
         
-        this.runSpeed = runSpeed;
-     }
-     run(){
-          console.log(`this ${ this.name} can run`)
-          super.move(this.runSpeed)
-     }
-
+        }
+        else{
+            console.error('first name must be a non empty strring')
+        }
+    }
+    set lastName(newLastName) {
+        if(typeof newLastName === 'string' && newLastName.length > 0){
+             this._LastName= newLastName;
+        
+        }
+        else{
+            console.error('last name must be a non empty strring')
+        }
+    }
+    set age(newAge) {
+        if(typeof newAge === 'number' && newAge >= 0) {
+          this._age = newAge;
+        }
+        else{
+            console.error('age must be a non negative number')
+        }
+    }
+    get firstName() {
+        return this._firstName
+    }
+    get lastName() {
+        return this._LastName
+    }
+    get fullName(){
+        return this._firstName + ' ' + this._LastName;
+    }
+    get age(){
+        return this._age
+    }
+    
 }
-class Fish extends Animal{
-      constructor(name, age, swimSpeed){
-          super(name, age);
-         
-          this.swimSpeed = swimSpeed;
-       }
-       swim(){
-          console.log(`this ${ this.name} can swim`);
-          super.move(this.swimSpeed)
+const person = new Person(' boz', 'sef', 40)
 
-     }
-}
-class Hawk extends Animal{
-     constructor(name, age, flySpeed){
-          super(name, age);
-          
-          this.flySpeed = flySpeed;
-       }
-       fly(){
-          console.log(`this ${ this.name} can fly`);
-          super.move(this.flySpeed)
-
-     }
-}
-
-const rabbit = new Rabit('rabbit', 1, 25);
-const fish = new Fish('fish', 2, 5);
-const hawk = new Hawk('hawk', 3, 30);
-rabbit.run();
-fish.swim();
-hawk.fly()
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.fullName);
+console.log(person.age);
 
 
