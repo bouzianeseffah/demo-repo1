25426910704724b49f6987ec14039657ef2a8eac
bouzianes <fcbdreams1991@gmail.com>
2024-08-31 09:34:40 -0400

    nested object

--- index.js
+++ index.js
@@ -1,54 +1,21 @@
 class Person {
-    constructor(firstName, lastName, age) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+    constructor(name, age, ...address){
+        this.name = name;
         this.age = age;
+        this.address = new Address(...address)
+
     }
-    set firstName(newFistName) {
-        if(typeof newFistName === 'string' && newFistName.length > 0){
-             this._firstName = newFistName;
-        
-        }
-        else{
-            console.error('first name must be a non empty strring')
-        }
-    }
-    set lastName(newLastName) {
-        if(typeof newLastName === 'string' && newLastName.length > 0){
-             this._LastName= newLastName;
-        
-        }
-        else{
-            console.error('last name must be a non empty strring')
-        }
-    }
-    set age(newAge) {
-        if(typeof newAge === 'number' && newAge >= 0) {
-          this._age = newAge;
-        }
-        else{
-            console.error('age must be a non negative number')
-        }
-    }
-    get firstName() {
-        return this._firstName
-    }
-    get lastName() {
-        return this._LastName
-    }
-    get fullName(){
-        return this._firstName + ' ' + this._LastName;
-    }
-    get age(){
-        return this._age
-    }
-    
 }
-const person = new Person(' boz', 'sef', 40)
+class Address {
+    constructor(street, city, country){
+          this.street = street;
+          this.city = city;
+          this.country = country
+    }
+}
+const person1 = new Person('boz', 30, '6926 5th', 'brooklyn', 'us');
+console.log(person1.address.city)
 
-console.log(person.firstName);
-console.log(person.lastName);
-console.log(person.fullName);
-console.log(person.age);
-
-
+for(const  property in person1.address){
+    console.log(person1.address[property])
+}
