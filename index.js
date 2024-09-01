3d225dc214e0fdf3c6e0e071043b6c49867e4022
bouzianes <fcbdreams1991@gmail.com>
2024-09-01 09:38:25 -0400

    array of objects

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
-class Person {
-    constructor(name, age, ...address){
-        this.name = name;
-        this.age = age;
-        this.address = new Address(...address)
+const fruits = [{name: 'apple', color: 'red', colories: 95 } ,
+    {name: 'orange', color: 'orange', colories: 45}, 
+    {name: 'banana', color: 'yellow', colories: 105}, 
+    {name: 'coconut', color: 'white', colories: 159},
+    {name: 'pineapple', color: 'yellow', colories: 37}]
 
-    }
-}
-class Address {
-    constructor(street, city, country){
-          this.street = street;
-          this.city = city;
-          this.country = country
-    }
-}
-const person1 = new Person('boz', 30, '6926 5th', 'brooklyn', 'us');
-console.log(person1.address.city)
+fruits.push({name: 'grapes', color: 'purple', colories: '95'})        
+fruits.forEach(fruit => console.log(fruit.name))
+const fruitNames = fruits.map(fruit => fruit.name )
+const fruitColors = fruits.map(fruit => fruit.color )
 
-for(const  property in person1.address){
-    console.log(person1.address[property])
-}
+console.log(fruitNames);
+console.log(fruitColors);
+
+const yellow = fruits.filter(fruit => fruit.color === 'yellow');
+
+console.log(yellow)
+
+const lowCalFruits = fruits.filter(fruit => fruit.colories < 100);
+
+console.log(lowCalFruits)
+
+const maxFruits = fruits.reduce((max, fruit) => 
+                 fruit.colories > max.colories ?
+                 fruit : max)
+
+console.log(maxFruits)                 
+                 
+const minFruits = fruits.reduce((min, fruit) => 
+    fruit.colories < min.colories ?
+    fruit : min)
+
+console.log(minFruits)    
+
