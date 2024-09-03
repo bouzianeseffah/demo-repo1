f838552a85c9bf46eeb8cddaaba81d43bb075a99
bouzianes <fcbdreams1991@gmail.com>
2024-09-03 16:16:43 -0400

    shuffle algorithm

--- index.js
+++ index.js
@@ -1,34 +1,12 @@
-const fruits = [{name: 'apple', color: 'red', colories: 95 } ,
-    {name: 'orange', color: 'orange', colories: 45}, 
-    {name: 'banana', color: 'yellow', colories: 105}, 
-    {name: 'coconut', color: 'white', colories: 159},
-    {name: 'pineapple', color: 'yellow', colories: 37}]
+//shuffle algorithm
+const cards = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+shuffle(cards)
+console.log(cards) 
+function shuffle(array){
+    for(let i = array.length - 1; i > 0; i-- ){
+        const random =Math.floor( Math.random() * (i + 1));
+        [array[i], array[random]] = [array[random], array[i]] 
+    }
 
-fruits.push({name: 'grapes', color: 'purple', colories: '95'})        
-fruits.forEach(fruit => console.log(fruit.name))
-const fruitNames = fruits.map(fruit => fruit.name )
-const fruitColors = fruits.map(fruit => fruit.color )
-
-console.log(fruitNames);
-console.log(fruitColors);
-
-const yellow = fruits.filter(fruit => fruit.color === 'yellow');
-
-console.log(yellow)
-
-const lowCalFruits = fruits.filter(fruit => fruit.colories < 100);
-
-console.log(lowCalFruits)
-
-const maxFruits = fruits.reduce((max, fruit) => 
-                 fruit.colories > max.colories ?
-                 fruit : max)
-
-console.log(maxFruits)                 
-                 
-const minFruits = fruits.reduce((min, fruit) => 
-    fruit.colories < min.colories ?
-    fruit : min)
-
-console.log(minFruits)    
+}
 
