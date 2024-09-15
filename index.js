8c92cf93f4580a36b7f5ecbd701e0d60b6a79b33
bouzianes <fcbdreams1991@gmail.com>
2024-09-15 09:39:15 -0400

    error object

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 
-function func1(callback){
-    setTimeout(() =>{console.log('task1');
-                       callback()}, 3000)
+try{
+    
+const dividend =Number (window.prompt('enter a dividend:'));
+const divisor = Number (window.prompt('enter a divisor:'));
+if(divisor == 0){
+  throw new Error ('you cant divide by zero!')
 }
-function func2(){
-    console.log('1')
-    console.log('2')
-    console.log('3')
+if (isNaN(dividend )|| isNaN(divisor)) {
+    throw new Error('values must be a number')
+    
+}
+const result = dividend / divisor;
+console.log(result)
+}
+catch(error){
+    console.error(error)
 
 }
-func1(func2)
+console.log('hou have reached the end')
