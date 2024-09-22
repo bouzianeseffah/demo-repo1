36e072fd9ae79394e8cc717f60b54e8392768486
bouzianes <fcbdreams1991@gmail.com>
2024-09-22 09:01:11 -0400

    add & change html

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const display = document.getElementById('display');
+ //create the  element
 
-function appendToDisplay(input){
-      display.value += input;
-}
-function clearDisplay(){
-    display.value = '';
+ const newH1 = document.createElement('h1');
 
-}
-function calculate(){
-    try {
-        display.value = eval(display.value)
+ newH1.textContent = 'i like pizza';
+newH1.id = 'myH1'
+newH1.style.color = 'red';
+newH1.style.textAlign = 'center'
+ document.body.append(newH1)
+document.getElementById('box1').append(newH1)
+//const box2 = document.getElementById('box2');
+//document.body.insertBefore(newH1, box2)
 
-    } catch (error) {
-        display.value = 'Error'
-    }
+// if box dont have id
+//const boxes  = document.querySelectorAll('.box')
+//document.body.insertBefore(newH1, boxes[1])
 
-}
+
+//remove html element
+
+document.getElementById('box1').removeChild(newH1)
