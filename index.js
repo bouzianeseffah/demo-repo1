eec668fc1647ad18a811333f82f201336aa1a2c6
bouzianes <fcbdreams1991@gmail.com>
2024-09-22 09:21:40 -0400

    add & change html 2

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
- //create the  element
 
- const newH1 = document.createElement('h1');
+const newListItem = document.createElement('li');
+newListItem.textContent = 'coconut';
+newListItem.id = 'coconut';
+newListItem.style.fontWeight = 'bold'
+newListItem.style.background = 'lightgreen'
 
- newH1.textContent = 'i like pizza';
-newH1.id = 'myH1'
-newH1.style.color = 'red';
-newH1.style.textAlign = 'center'
- document.body.append(newH1)
-document.getElementById('box1').append(newH1)
-//const box2 = document.getElementById('box2');
-//document.body.insertBefore(newH1, box2)
+//document.body.append(newListItem)
+//document.getElementById('fruits').prepend(newListItem);
 
-// if box dont have id
-//const boxes  = document.querySelectorAll('.box')
-//document.body.insertBefore(newH1, boxes[1])
+//document.getElementById('orange').append(newListItem);
+//const orange = document.getElementById('orange');
+
+//document.getElementById('fruits').insertBefore(newListItem, orange)
 
 
-//remove html element
+//if dont have id
 
-document.getElementById('box1').removeChild(newH1)
+const listItem = document.querySelectorAll('#fruits li ');
+
+document.getElementById('fruits').insertBefore(newListItem, listItem[2])
+
+document.getElementById('fruits').removeChild(newListItem)
