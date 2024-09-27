c85f2d66c26c716e52b329ab39736fbc626f36e5
bouzianes <fcbdreams1991@gmail.com>
2024-09-27 09:42:54 -0400

    nodeLists

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
-const myBtn = document.getElementById('myBtn');
-const myImg = document.getElementById('myImg');
-
-myBtn.addEventListener('click', event => {
-    if(myImg.style.visibility === 'hidden') {
-        myImg.style.visibility = 'visible';
-        myBtn.textContent = 'Hide';
+let buttons = document.querySelectorAll('.myBtns');
+//NodeList
+//console.log(buttons)
 
 
-    }else{
-        myImg.style.visibility = 'hidden';
-        myBtn.textContent = 'Show'
+//add HTML/CSS properties
+// buttons.forEach(button => {
+//     button.style.backgroundColor = 'green';
+//     button.textContent += '😁'
 
-    }
+// })
+
+
+// CLICK event listener
+// buttons.forEach(button => {
+//    button.addEventListener('click' , event => {
+//       event.target.style.backgroundColor = 'red'
+//    })
+// })
+
+//mouseover, mouseout
+// buttons.forEach(button => {
+//     button.addEventListener('mouseover' , event => {
+//         event.target.style.backgroundColor = 'red'
+//     })
+// })
+// buttons.forEach(button => {
+//     button.addEventListener('mouseout' , event => {
+//         event.target.style.backgroundColor= 'rgb(173, 173, 247)';
+
+//     });
+
+//add an Element
+
+// const newButton = document.createElement('button');
+// newButton.textContent = 'Button5';
+// newButton.classList = 'myBtns';
+// document.body.appendChild(newButton);
+// console.log(buttons);// only 4 button 
+// buttons = document.querySelectorAll('.myBtns')
+
+// console.log(buttons)//5 buttons
+
+//remove an element
+
+buttons.forEach(button => {
+    button.addEventListener('click', event => {
+        event.target.remove();
+        //console.log(buttons)//style 4 buttons
+        buttons = document.querySelectorAll('.myBtns')
+        console.log(buttons) 
+    })
 })
