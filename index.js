4087b6e9b15bce2a2116ae7ba65134ed3e172eda
bouzianes <fcbdreams1991@gmail.com>
2024-09-29 23:29:31 -0400

    rock paper scissors game

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
-let buttons = document.querySelectorAll('.myButtons');
+const choices = ['rock', 'paper', 'scissors'];
+const playerDisplay = document.getElementById('playerDisplay');
+const computerDisplay = document.getElementById('computerDisplay');
+const resultDisplay = document.getElementById('resultDisplay');
+const playerScoreDisplay = document.getElementById('playerScoreDisplay');
+const computerScoreDisplay = document.getElementById('computerScoreDisplay');
+let playerScore = 0;
+let computerScor = 0;
+function playGame(playerChoice){
+    const computerChoice = choices[Math.floor( Math.random() * 3)];
+    //console.log(computerChoice)
+    let result = '';
+    if(playerChoice === computerChoice){
+        result = 'it a tie';
+    }
+    else{
+        switch(playerChoice){
+            case 'rock':
+                result = (computerChoice === 'scissors') ? 'you win!' : 'you lose!';
+                break;
+            case 'paper':
+                 result = (computerChoice === 'rock') ? 'you win!' : 'you lose!';
+                break; 
+            case 'scissors':
+                 result = (computerChoice === 'paper') ? 'you win!' : 'you lose!';
+                break;       
 
-buttons.forEach(button => {
-  button.classList.add('enabled');
-});
-
-buttons.forEach(button => {
-    button.addEventListener('mouseover' , event => {
-        event.target.classList.toggle('hover');
-    });
-});
-buttons.forEach(button => {
-    button.addEventListener('mouseout' , event => {
-        event.target.classList.toggle('hover');
-    });
-});
-
-
-buttons.forEach(button => {
-    button.addEventListener('click', event => {
-
-        if(event.target.classList.contains('disabled')){
-            event.target.textContent += '😥';
         }
-        else{
-            event.target.classList.replace('enabled', 'disabled')
+     
+    }    
+    playerDisplay.textContent = `PLAYER : ${playerChoice}`;
+    computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
+    resultDisplay.textContent = result;
+    //reset the color back to the original
+    resultDisplay.classList.remove('greenText', 'redText')
+    switch(result){
+        case 'you win!':
+            resultDisplay.classList.add('greenText');
+            playerScore++;
+            playerScoreDisplay.textContent = playerScore;
+            break;
+        case 'you lose!':
+            resultDisplay.classList.add('redText');
+            computerScor++
+            computerScoreDisplay.textContent = computerScor;
+            break ;  
+    }
 
-        }
-    })
-})
+}
