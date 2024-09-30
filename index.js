c0a291edbf8ebe54b4608e4becf7b240e7ea2064
bouzianes <fcbdreams1991@gmail.com>
2024-09-30 09:45:49 -0400

    img slider

--- index.js
+++ index.js
@@ -1,49 +1,35 @@
-const choices = ['rock', 'paper', 'scissors'];
-const playerDisplay = document.getElementById('playerDisplay');
-const computerDisplay = document.getElementById('computerDisplay');
-const resultDisplay = document.getElementById('resultDisplay');
-const playerScoreDisplay = document.getElementById('playerScoreDisplay');
-const computerScoreDisplay = document.getElementById('computerScoreDisplay');
-let playerScore = 0;
-let computerScor = 0;
-function playGame(playerChoice){
-    const computerChoice = choices[Math.floor( Math.random() * 3)];
-    //console.log(computerChoice)
-    let result = '';
-    if(playerChoice === computerChoice){
-        result = 'it a tie';
+const slides = document.querySelectorAll('.slides img');
+let slidesIndex = 0;
+let intervalId = null;
+//initializeSlider();
+document.addEventListener('DOMContentLoaded',initializeSlider);
+function initializeSlider(){
+    if(slides.length > 0){
+        slides[slidesIndex].classList.add('displaySlide');
+        intervalId =  setInterval(nextSlide, 5000);
+        //console.log(intervalId);
     }
-    else{
-        switch(playerChoice){
-            case 'rock':
-                result = (computerChoice === 'scissors') ? 'you win!' : 'you lose!';
-                break;
-            case 'paper':
-                 result = (computerChoice === 'rock') ? 'you win!' : 'you lose!';
-                break; 
-            case 'scissors':
-                 result = (computerChoice === 'paper') ? 'you win!' : 'you lose!';
-                break;       
-
-        }
-     
-    }    
-    playerDisplay.textContent = `PLAYER : ${playerChoice}`;
-    computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
-    resultDisplay.textContent = result;
-    //reset the color back to the original
-    resultDisplay.classList.remove('greenText', 'redText')
-    switch(result){
-        case 'you win!':
-            resultDisplay.classList.add('greenText');
-            playerScore++;
-            playerScoreDisplay.textContent = playerScore;
-            break;
-        case 'you lose!':
-            resultDisplay.classList.add('redText');
-            computerScor++
-            computerScoreDisplay.textContent = computerScor;
-            break ;  
+   
+}
+function showSlide(index){
+    if(index >= slides.length){
+       slidesIndex = 0
     }
+    else if (index < 0){
+      slidesIndex = slides.length -1;
+    }
+   slides.forEach(slide => {
+    slide.classList.remove('displaySlide');
+   })
+   slides[slidesIndex].classList.add('displaySlide')
+}
+function prevSlide(){
+    clearInterval(intervalId)
+    slidesIndex--;
+    showSlide(slidesIndex);
 
+}
+function nextSlide(){
+    slidesIndex++
+    showSlide(slidesIndex)
 }
